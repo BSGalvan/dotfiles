c5405591c5fe4bfa7b3eaefb610f0273a4de6753
Bharath Saiguhan <bharathsaiguhan@protonmail.com>
2022-10-23 00:42:58 +0530

    feat(emacs): setup org-mode, org-roam, org-roam-ui
    Pretty big change, with configurations for org-mode, org-roam and
    org-roam-ui.
    NOTE: See the code for the related shortcuts, and which folders
    are expected to be created for storing .org, and org-roam files.
    
    Additional changes are as follows:
    - set up ligature.el, which is (now) available on MELPA.
    - add configuration for org-bullets, for those sweet looking org-mode
    lists
    - customize the font faces for various things. NOTE: you will need the
    Indian Font Foundry's General Sans, and Chubbo for this to render
    as intended. Put these in /usr/share/fonts/non-free and you're golden.
    - couple of shortcuts added to enable telescope-like file finding and
    navigation
    - line numbers were disabled for some modes (see code for exact config)

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -19,6 +19,13 @@
 (setq inhibit-startup-message t  ; Don't display splash screen
       visible-bell t)            ; Flash screen for attention
 
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
 ;; Use M-x recentf-open-files to see recently opened files
 (recentf-mode 1)
 
@@ -34,8 +41,9 @@
 (load custom-file 'noerror 'nomessage)
 
 ;; Customize font settings
-(set-face-attribute 'default nil :family "FiraCode Nerd Font" :height 140)
-(set-face-attribute 'fixed-pitch nil :family "FiraCode Nerd Font Mono" :height 120)
+(set-face-attribute 'default nil :family "FiraCode Nerd Font" :height 150)
+(set-face-attribute 'fixed-pitch nil :family "FiraCode Nerd Font Mono" :height 150)
+(set-face-attribute 'variable-pitch nil :family "General Sans" :height 160)
 
 ;; Don't pop up UI dialogs when prompting
 (setq use-dialog-box nil)
@@ -104,8 +112,8 @@
 
 ;; Add in doom-themes because why not
 (use-package doom-themes
-  :custom (doom-themes-visual-bell-config)
-  :init (load-theme 'doom-vibrant t))
+  :init (load-theme 'doom-tokyo-night t)
+  :config (doom-themes-visual-bell-config))
 
 ;; Icons everywhere!
 (use-package all-the-icons
@@ -148,7 +156,10 @@
     :global-prefix "C-SPC")
 
   (bsg/leader-keys
-    "tt" '(counsel-load-theme :which-key "choose theme")))
+    "tt" '(counsel-load-theme :which-key "choose theme")
+    "fb" '(counsel-switch-buffer :which-key "find buffer")
+    "fe" '(counsel-find-file :which-key "find file")
+    "fo" '(counsel-recentf :which-key "find old file")))
 
 ;; "Here's a little lesson in trickery..."
 
@@ -186,3 +197,112 @@
 
 (bsg/leader-keys
   "ts" '(hydra-text-scale/body :which-key "scale text"))
+
+(defun bsg/org-mode-setup ()
+  (org-indent-mode)
+  (auto-fill-mode 0)
+  (visual-line-mode 1)
+  (variable-pitch-mode 1)
+  (setq org-toggle-pretty-entities 1)
+  (setq evil-auto-indent nil))
+
+(defun bsg/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.25)
+                  (org-level-2 . 1.20)
+                  (org-level-3 . 1.15)
+                  (org-level-4 . 1.10)
+                  (org-level-5 . 1.05)))
+    (set-face-attribute (car face) nil :font "Chubbo" :weight 'bold :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+
+(use-package org
+  :hook (org-mode . bsg/org-mode-setup)
+  :config
+  (setq org-ellipsis " ⮟"
+	org-hide-emphasis-markers nil)
+  (bsg/org-font-setup))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun bsg/org-mode-visual-fill ()
+  (setq visual-fill-column-width 120
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . bsg/org-mode-visual-fill))
+
+(use-package ligature
+  :config
+  ;; Enable the "www" ligature in every possible major mode
+  (ligature-set-ligatures 't '("www"))
+  ;; Enable traditional ligature support in eww-mode, if the
+  ;; `variable-pitch' face supports it
+  (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
+  ;; Enable all Cascadia Code ligatures in programming modes
+  (ligature-set-ligatures 'prog-mode '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
+                                       ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
+                                       "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
+                                       "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
+                                       "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
+                                       "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
+                                       "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
+                                       "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
+                                       ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
+                                       "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
+                                       "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
+                                       "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
+                                       "\\\\" "://"))
+  ;; Enables ligature checks globally in all buffers. You can also do it
+  ;; per mode with `ligature-mode'.
+  (global-ligature-mode t))
+
+(use-package org-roam
+  :custom
+  (org-roam-directory "~/notes/roam-notes")
+  (org-roam-capture-templates
+   '(("d" default plain
+      "%?"
+      :if-new (file+head "${slug}.org" "#+title: ${title}\n#+date: %U\n")
+      :unarrowed t)
+     ("l" "prog-lang" plain
+      (file ~/notes/roam-notes/Templates/ProgLang.org)
+      :if-new (file+head "${slug}.org" "#+title: ${title}\n#+date: %U\n")
+      :unarrowed t)))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+	 ("C-c n f" . org-roam-node-find)
+	 ("C-c n i" . org-roam-node-insert)
+	 :map org-mode-map
+	 ("C-M-i" . completion-at-point))
+  :config
+  (org-roam-setup))
+
+(use-package org-roam-ui
+  :after org-roam
+  ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+  ;;         a hookable mode anymore, you're advised to pick something yourself
+  ;;         if you don't care about startup time, use
+  ;;  :hook (after-init . org-roam-ui-mode)
+  :config
+  (setq org-roam-ui-sync-theme t
+        org-roam-ui-follow t
+        org-roam-ui-update-on-save t
+        org-roam-ui-open-on-start t))
