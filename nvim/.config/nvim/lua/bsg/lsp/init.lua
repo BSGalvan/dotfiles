a716dba23c17d5622178bd728571ac149aa1b49e
Bharath Saiguhan <bharathsaiguhan@protonmail.com>
2022-10-23 00:20:43 +0530

    fix(cmp_nvim_lsp)!: swap out deprecated api
    As per hrsh7th/cmp-nvim-lsp/issues/38#issuecomment-1279744539,
    update_capabilities is now deprecated.
    
    Additionally, add a few personal dictionary words as well.

--- nvim/.config/nvim/lua/bsg/lsp/init.lua
+++ nvim/.config/nvim/lua/bsg/lsp/init.lua
@@ -113,7 +113,7 @@ local function make_config(server_name)
     },
   }
 
-  capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+  capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
   local config = {
     -- enable all the stuff above + snippets
